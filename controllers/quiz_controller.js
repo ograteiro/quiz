009b795cf357d6a9bcb85c3705f0015c27f86e86
OGrateiro <o.grateiro@gmail.com>
2015-08-02 19:21:26 +0200

    Add commentaries support

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,22 @@
 var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
+// Autoload :id
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.find(quizId).then(
-		function(quiz) {
-			if(quiz) {
-				req.quiz = quiz;
-				next();
-			} else { next(new Error('No existe quizId ' + quizId));}
-		}).catch(function(error) {next(error);});
- 
+  models.Quiz.find({
+            where: {
+                id: Number(quizId)
+            },
+            include: [{
+                model: models.Comment
+            }]
+        }).then(function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{next(new Error('No existe quizId=' + quizId))}
+    }
+  ).catch(function(error){next(error)});
 };
-
 
 // GET /quizes?search=texto_a_buscar
 exports.index = function(req, res) {
--- models/models.js
+++ models/models.js
@@ -29,11 +29,20 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
-// Importar definicion de la tabla Quiz
+// Importar definicion de las tablas Quiz y Comments
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
-exports.Quiz = Quiz; // exportar tabla Quiz
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+// Modelar la relacion 1 a N entre pregunta y comentarios
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+// Exportar tablas
+exports.Quiz = Quiz; 
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
