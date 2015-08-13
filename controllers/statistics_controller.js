c33bef892ace270300b584c3234546019fda0a94
OGrateiro <o.grateiro@gmail.com>
2015-08-13 13:57:19 +0200

    First implementation with statistics

---
+++ controllers/statistics_controller.js
@@ -0,0 +1,40 @@
+var models = require('../models/models.js');
+
+exports.show = function(req, res) {
+    // TODO Consultar todos los datos y devolverlos como parametro en el res.rend
+    var statistics = {nQueries: {name: "Numero de preguntas", value: 0 }, 
+                      nComments: {name: "Numero de comentarios", value: 0},
+                      avgCommentsPerQuestion: {name: "Media de comentarios por pregunta", value: 0},
+		      nQueriesWithComments: {name: "Numero de preguntas con comentarios", value: 0},
+		      nQueriesWithoutComments: {name: "Numero de preguntas sin comentarios", value: 0 }
+                     };
+    
+    // Access database
+    models.Quiz.count().then(function(nQueries){
+				statistics.nQueries.value = nQueries;
+				console.log("Number of queries:" + nQueries);
+				return models.Comment.count();
+			}).
+			then(function(nComments){
+				statistics.nComments.value = nComments;
+				console.log("Number of comments:" + nComments);
+				return models.Quiz.count({include: [{model: models.Comment, required: true}]});
+			}).
+			then(function (nQueriesWithComm){
+				statistics.nQueriesWithComments.value = nQueriesWithComm;
+				console.log("Number of queries with comments:" + nQueriesWithComm);
+			}).
+			catch(function(error){
+				console.log(error);
+			}).
+			finally(function(){
+				if( statistics.nQueries.value !== 0 ){
+					statistics.avgCommentsPerQuestion.value = 
+						statistics.nComments.value / statistics.nQueries.value;
+				}
+				statistics.nQueriesWithoutComments.value = statistics.nQueries.value -
+					statistics.nQueriesWithComments.value;
+				res.render('statistics/show', { statistics: statistics, errors: []});
+			});
+		
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var statisticsController = require('../controllers/statistics_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -36,6 +38,8 @@ router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
 	                                    sessionController.loginRequired, commentController.publish);
 
+/* Comments management */
+router.get('/quizes/statistics', statisticsController.show);
 
 /* GET credits page */
 router.get('/author', function(req, res) {
